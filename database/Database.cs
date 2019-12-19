cb384bca16968da69a71fdfd3e9165f1f2d62ff7
augarkovic <alen.ugarkovic13497@gmail.com>
2019-12-19 17:46:47 +0100

    02 - Dodane klase MyDatabase i Database u database modul. Implementirane funkcije za dohvat i spremanje podataka u bazu.

---
+++ database/Database.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace database
+{
+    public abstract class Database
+    {
+        static MyDatabase database;
+
+        public static database.MyDatabase DatabasePath
+        {
+            get
+            {
+                if (database == null)
+                    database = new MyDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "database.db3"));
+                return database;
+            }
+        }
+    }
+}
---
+++ database/MyDatabase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SQLite;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using database.Entities;
+
+namespace database
+{
+    public class MyDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public MyDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Store>().Wait();
+            _database.CreateTableAsync<Discount>().Wait();
+        }
+
+        public Task<List<Store>> GetStores()
+        {
+            return _database.Table<Store>().ToListAsync();
+        }
+
+        public Task<List<Discount>> GetDiscounts()
+        {
+            return _database.Table<Discount>().ToListAsync();
+        }
+
+        public Task<int> InsertStores(Store store)
+        {
+            return _database.InsertAsync(store);
+        }
+
+        public Task<int> InsertDiscounts(Discount discount)
+        {
+            return _database.InsertAsync(discount);
+        }
+
+        public Task<List<Discount>> GetDiscountByStoreId(int storeID)
+        {
+            return _database.QueryAsync<Discount>("SELECT * FROM Discount WHERE storeId = " + storeID);
+        }
+    }
+}
