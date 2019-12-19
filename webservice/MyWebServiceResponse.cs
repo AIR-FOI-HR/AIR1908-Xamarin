8eaebcae86cbb68dd23c5bfff881ba1947477870
augarkovic <alen.ugarkovic13497@gmail.com>
2019-12-19 22:51:38 +0100

    03 - Dodane klase MyWebServiceCaller i MyWebServiceResponse. Dodan interface MyWebService.

---
+++ webservice/MyWebServiceResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webservice
+{
+    public class MyWebServiceResponse
+    {
+        public int ResponseId { get; set; }
+        public String ResponseText { get; set; }
+        public long TimeStamp { get; set; }
+        public String Items { get; set; }
+    }
+}
