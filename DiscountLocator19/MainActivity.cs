54915092fb16027336efc2534d3599a6a30740de
augarkovic <alen.ugarkovic13497@gmail.com>
2019-12-19 21:48:49 +0100

    02 - Prikazani popusti iz MockData klase u ListView-u na on click listener u MainActivity.

--- DiscountLocator19/MainActivity.cs
+++ DiscountLocator19/MainActivity.cs
@@ -4,15 +4,20 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 using System;
+using System.Linq;
 using database.Entities;
 using System.Collections.Generic;
 using database;
+using Android.Views;
+using Android.Support.V7.RecyclerView.Extensions;
 
 namespace DiscountLocator19
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button button;
+        ListView listView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -21,6 +26,20 @@ namespace DiscountLocator19
             SetContentView(Resource.Layout.activity_main);
 
             mockData();
+
+            button = FindViewById<Button>(Resource.Id.test_button);
+            listView = FindViewById<ListView>(Resource.Id.discount_list);
+            button.Click += delegate{ onButtonClick(); };
+        }
+
+        private void onButtonClick()
+        {
+            List<Discount> discounts = Database.DatabasePath.GetDiscounts().Result;
+
+            List<String> names = discounts.Select(d => d.Name).ToList();
+
+            ArrayAdapter listAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, names);
+            listView.Adapter = listAdapter;
         }
 
         private void mockData()
