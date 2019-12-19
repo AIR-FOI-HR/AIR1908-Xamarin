4336910d8af5e43a0551240e4ad92ed7b6fb919e
augarkovic <alen.ugarkovic13497@gmail.com>
2019-12-19 23:20:14 +0100

    03 - Implementirana getAll metoda za dohvat dućana s web servisa.

--- webservice/MyWebServiceCaller.cs
+++ webservice/MyWebServiceCaller.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using database.Entities;
+using Refit;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,14 +10,35 @@ namespace webservice
     {
         private const String baseUrl = "http://cortex.foi.hr/mtl/courses/air/";
 
+        private static MyWebService _api;
+
         public MyWebServiceCaller()
         {
 
         }
 
-        public async void getAll(Dictionary<String, String>, Type entityType)
+        public async void getAll(Dictionary<String, String> method, Type entityType)
         {
+            _api = RestService.For<MyWebService>(baseUrl);
 
+            var callStores = await _api.getStores(method);
+            var callDiscount = await _api.getDiscounts(method);
+
+            if (callStores != null)
+            {
+                if (entityType == typeof(Store))
+                {
+                    Console.WriteLine("Got stores!");
+                }
+                else if (entityType == typeof(Discount))
+                {
+                    Console.WriteLine("Got discounts!");
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized class.");
+                }
+            }
         }
     }
 }
