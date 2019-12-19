0d41089fc19a52b8c3e76b44a84cb827a2339c38
augarkovic <alen.ugarkovic13497@gmail.com>
2019-12-19 17:17:02 +0100

    02 - Dodane klase Store i Discount. Dodani pripadajući atributi.

---
+++ database/Entities/Discount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace database.Entities
+{
+    public class Discount
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [ForeignKey(typeof(Store))]
+        public int storeId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int discount { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
+    }
+}
---
+++ database/Entities/Store.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace database.Entities
+{
+    public class Store
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public long Latitude { get; set; }
+
+        public long Longitude { get; set; }
+    }
+}
