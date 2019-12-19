62eb1366e09bf139f90b661d58b7388287b63d16
augarkovic <alen.ugarkovic13497@gmail.com>
2019-12-19 20:49:43 +0100

    02 - Dodana MockData klasa. Dodan gumb i ListView u activity_main.xml.

---
+++ database/MockData.cs
@@ -0,0 +1,34 @@
+﻿using database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace database
+{
+    public class MockData
+    {
+        private static MyDatabase database;
+        public static void writeAll()
+        {
+           
+            Store store = new Store();
+            store.Name = "Ducan1";
+            store.ID = Database.DatabasePath.InsertStores(store).Result;
+
+            Discount discount = new Discount();
+            discount.Name = "Popust 1";
+            discount.Description = "Badjune ovo cu ti zapamtit";
+            discount.storeId = store.ID;
+
+            Database.DatabasePath.InsertDiscounts(discount);
+
+
+            Discount discount2 = new Discount();
+            discount2.Name = "Popust 2";
+            discount2.Description = "Badjune ovo cu ti necu zaboravit";
+            discount2.storeId = store.ID;
+
+            Database.DatabasePath.InsertDiscounts(discount2);
+        }
+    }
+}
