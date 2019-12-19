e3648aeb8cd44273cc725076fe5d248b523349b4
augarkovic <alen.ugarkovic13497@gmail.com>
2019-12-19 23:34:11 +0100

    03 - Dodane handleStores i handleDiscounts metode. Deserijalizacija odgovora u svakoj od njih.

--- webservice/MyWebServiceCaller.cs
+++ webservice/MyWebServiceCaller.cs
@@ -1,4 +1,5 @@
 ﻿using database.Entities;
+using Newtonsoft.Json;
 using Refit;
 using System;
 using System.Collections.Generic;
@@ -22,23 +23,38 @@ namespace webservice
             _api = RestService.For<MyWebService>(baseUrl);
 
             var callStores = await _api.getStores(method);
-            var callDiscount = await _api.getDiscounts(method);
+            var callDiscounts = await _api.getDiscounts(method);
 
             if (callStores != null)
             {
                 if (entityType == typeof(Store))
                 {
                     Console.WriteLine("Got stores!");
+                    handleStores(callStores);
                 }
                 else if (entityType == typeof(Discount))
                 {
                     Console.WriteLine("Got discounts!");
+                    handleDiscounts(callDiscounts);
                 }
                 else
                 {
                     Console.WriteLine("Unrecognized class.");
                 }
             }
+        }
+
+        private void handleDiscounts(MyWebServiceResponse responseStores)
+        {
+            Store[] storeItems = JsonConvert.DeserializeObject<Store[]>(responseStores.Items);
+        }
+
+        private void handleStores(MyWebServiceResponse responseDiscounts)
+        {
+            Discount[] discountItems = JsonConvert.DeserializeObject<Discount[]>(responseDiscounts.Items, new JsonSerializerSettings 
+            { 
+                DateFormatString = "yyyy-MM-dd"
+            });
         }
     }
 }
