9ac3e5c0db91a2678b6b54f25e1b1bb5588506cb
kchunter <matija.zinic15@gmail.com>
2019-12-20 18:21:50 +0100

    04 - Napravljen override metode loadData u WsDataLoader klasi

--- DiscountLocator19/loaders/WsDataLoader.cs
+++ DiscountLocator19/loaders/WsDataLoader.cs
@@ -9,10 +9,31 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using core;
+using database.Entities;
+using webservice;
 
 namespace DiscountLocator19.loaders
 {
-    public class WsDataLoader
+    public class WsDataLoader : DataLoader
     {
+        public override void loadData(DataLoadedListener dataLoadedListener)
+        {
+            base.loadData(dataLoadedListener);
+
+            MyWebServiceCaller storesWs = new MyWebServiceCaller(storesHandler);
+            MyWebServiceCaller discountsWs = new MyWebServiceCaller(discountsHandler);
+            Dictionary<string, string> method = new Dictionary<string, string>();
+            method.Add("method", "getAll");
+            storesWs.getAll(method, typeof(Store));
+            discountsWs.getAll(method, typeof(Store));
+
+            MyWebServiceHandler storesHandler = null;
+
+            MyWebServiceHandler discountsHandler = null;
+
+            //Zasad još ne možemo pridružiti nikakvu vrijednost storesHandler i discountsHandler varijablama
+
+        }
     }
 }
