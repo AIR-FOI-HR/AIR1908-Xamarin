719f2cc22fc3fe314c4e2e63145551ce116fae18
kchunter <matija.zinic15@gmail.com>
2019-12-22 02:28:55 +0100

    05 - Dodana mapa naziva models i u njoj klasa TitleParent koja će nam služiti kao roditelj u expandable recyclerview elementu.

---
+++ DiscountLocator19/models/TitleParent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XamDroid.ExpandableRecyclerView;
+
+namespace DiscountLocator19.models
+{
+    public class TitleParent : IParentObject
+    {
+        Guid _id;
+        List<Object> _childrenList;
+
+        public TitleParent()
+        {
+            _id = Guid.NewGuid();
+        }
+
+        public Guid Id
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+
+            }
+        }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+
+        public List<object> ChildObjectList
+        {
+            get
+            {
+                return _childrenList;
+            }
+            set
+            {
+                _childrenList = value;
+            }
+        }
+    }
+}
