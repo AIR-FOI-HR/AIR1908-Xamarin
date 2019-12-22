7bc2ab1dbeb7e6e4087e1c83d77f13c44bcf1505
kchunter <matija.zinic15@gmail.com>
2019-12-22 03:11:14 +0100

    05 - Dodana mapa viewHolder te u njoj TitleParentViewHolder. U njoj povezujemo njezine varijable s elementima definiranima u store_list_item.xml.

---
+++ DiscountLocator19/viewHolder/TitleParentViewHolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XamDroid.ExpandableRecyclerView;
+
+namespace DiscountLocator19.viewHolder
+{
+    public class TitleParentViewHolder : ParentViewHolder
+    {
+        public TextView _textViewName;
+        public TextView _textViewDesc;
+        public ImageView _imageViewImg;
+        public TitleParentViewHolder(View itemView) : base(itemView)
+        {
+            _textViewName = itemView.FindViewById<TextView>(Resource.Id.store_name);
+            _textViewDesc = itemView.FindViewById<TextView>(Resource.Id.store_desc);
+            _imageViewImg = itemView.FindViewById<ImageView>(Resource.Id.store_image);
+        }
+    }
+}
