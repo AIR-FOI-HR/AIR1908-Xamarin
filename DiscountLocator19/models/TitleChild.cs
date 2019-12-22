db0411c75cdc58de5774fb5543ecdc9eb8bf315a
kchunter <matija.zinic15@gmail.com>
2019-12-22 02:32:12 +0100

    05 - U mapu models dodana klasa TitleChild koja će nam služiti kao dijete u expandable recyclerview elementu.

---
+++ DiscountLocator19/models/TitleChild.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DiscountLocator19.models
+{
+    public class TitleChild
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+
+        public TitleChild(string name, string description, int value)
+        {
+            Name = name;
+            Description = description;
+            Value = value;
+        }
+    }
+}
