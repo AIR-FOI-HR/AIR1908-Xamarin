41ab52190e4334f3d5a60f40614adbade56a8268
kchunter <matija.zinic15@gmail.com>
2019-12-22 03:01:19 +0100

    05 - U mapu models dodana je klasa TitleCreator. U njoj se instanciraju objekti klase TitleParent.

---
+++ DiscountLocator19/models/TitleCreator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using database.Entities;
+
+namespace DiscountLocator19.models
+{
+    public class TitleCreator
+    {
+        static TitleCreator _titleCreator;
+        List<TitleParent> _titleParents;
+
+        public TitleCreator(List<Store> stores)
+        {
+            _titleParents = new List<TitleParent>();
+
+            foreach (var store in stores)
+            {
+                var title = new TitleParent()
+                {
+                    Title = store.Name,
+                    Description = store.Description,
+                    ImgUrl = store.ImgUrl
+                };
+                _titleParents.Add(title);
+            }
+
+        }
+
+        public static TitleCreator Get(List<Store> stores)
+        {
+            if (_titleCreator == null)
+            {
+                _titleCreator = new TitleCreator(stores);
+            }
+            return _titleCreator;
+        }
+
+        public List<TitleParent> GetAll
+        {
+            get
+            {
+                return _titleParents;
+            }
+            private set
+            {
+
+            }
+        }
+    }
+}
