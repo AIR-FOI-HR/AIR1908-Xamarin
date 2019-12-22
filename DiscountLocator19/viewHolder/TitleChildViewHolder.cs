5372e78ade08fd1916d514227e7b793c858c13e9
kchunter <matija.zinic15@gmail.com>
2019-12-22 03:14:45 +0100

    05 - Dodana klasa TitleChildViewHolder u mapu viewHolder u kojoj njezine varijable povezujemo s elementima iz discount_list_item.xml.

---
+++ DiscountLocator19/viewHolder/TitleChildViewHolder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XamDroid.ExpandableRecyclerView;
+
+namespace DiscountLocator19.viewHolder
+{
+    public class TitleChildViewHolder : ChildViewHolder
+    {
+        public TextView name, description, value;
+
+        public TitleChildViewHolder(View itemView) : base(itemView)
+        {
+            name = itemView.FindViewById<TextView>(Resource.Id.discount_name);
+            description = itemView.FindViewById<TextView>(Resource.Id.discount_desc);
+            value = itemView.FindViewById<TextView>(Resource.Id.discount_value);
+        }
+    }
+}
