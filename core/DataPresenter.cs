0b40830fcc2aad18b88e6577337f70bcaecccddc
augarkovic <alen.ugarkovic13497@gmail.com>
2020-01-22 02:06:54 +0100

    08 - Kreiran DataPresenter interface u core modulu. Dodane metode u interface.

---
+++ core/DataPresenter.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core
+{
+    public interface DataPresenter
+    {
+        Drawable getIcon(Context context);
+        String getName(Context context);
+        [Obsolete]
+        Fragment getFragment();
+        void setData(List<Store> stores, List<Discount> discounts);
+    }
+}
