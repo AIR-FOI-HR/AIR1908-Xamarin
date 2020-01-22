1d6954de63e69c18000225a4e3ed29129bd56437
augarkovic <alen.ugarkovic13497@gmail.com>
2020-01-22 01:57:40 +0100

    08 - Ispravljene greške s AppCompat paketom.

--- DiscountLocator19/CurrentActivity.cs
+++ DiscountLocator19/CurrentActivity.cs
@@ -1,9 +1,17 @@
-﻿using Android.Support.V7.App;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
-namespace core
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace DiscountLocator19
 {
     public class CurrentActivity
     {
